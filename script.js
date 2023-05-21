9757e71dbfd09903664aa2b2cc9f0277276b20c0
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-05-21 22:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
 
+function swapTheme() {
+  var appDiv = document.getElementById('app');
+  var button = document.getElementById('swap');
+  
+  if (appDiv.classList.contains('day')) {
+    appDiv.classList.remove('day');
+    appDiv.classList.add('night');
+    button.classList.remove('button_day');
+    button.classList.add('button_night');
+  } else {
+    appDiv.classList.remove('night');
+    appDiv.classList.add('day');
+    
+    button.classList.remove('button_night');
+    button.classList.add('button_day');
+  }
+}
